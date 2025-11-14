13ca539ad3a3d3c05c788c1297c709a8eab5947f
Christoph Royer <mail@christophroyer.com>
2025-11-14 12:21:01 +0100

    A2.1 first transform done

--- tug_mr2/src/TransformNode.cpp
+++ tug_mr2/src/TransformNode.cpp
@@ -1,6 +1,7 @@
 #include "tug_mr2/TransformNode.hpp"
 
 #include <functional>
+#include <eigen3/Eigen/Core>
 
 namespace tug_mr2
 {
@@ -36,11 +37,52 @@ void TransformNode::laserCallback(const LaserScan::ConstSharedPtr& msg)
 {
   PointCloud pc;
   pc.header.stamp = get_clock()->now();
-  pc.header.frame_id = "odom";
+  pc.header.frame_id = "robot_0/mounting_plate";
 
   // TODO: Add your manual transformation calculation here
   //       You can add variables to the class if neded
+  float angle = msg->angle_min;
+  for (size_t i = 0; i < msg->ranges.size(); ++i) {
+    if (msg->ranges[i] >= msg->range_min && msg->ranges[i] <= msg->range_max) {
+      // base point: laser frame
+      Eigen::Vector4d pt = {
+        (float)msg->ranges[i] * cos(angle),
+        (float)msg->ranges[i] * sin(angle),
+        0.0,
+        1.0
+      };
 
+      // laser -> mounting plate
+      double phi = 84.0 / 180.0 * M_PI;
+      double theta = M_PI;
+      Eigen::MatrixX4d Translate_laser_mounting_plate{
+        {1, 0, 0, 0.04},
+        {0, 1, 0, 0},
+        {0, 0, 1, 0.05},
+        {0, 0, 0, 1},
+      };
+      Eigen::MatrixX4d Phi_laser_mounting_plate{
+        {cos(phi), -sin(phi), 0, 0},
+        {sin(phi), cos(phi), 0, 0},
+        {0, 0, 1, 0},
+        {0, 0, 0, 1}
+      };
+      Eigen::MatrixX4d Theta_laser_mounting_plate{
+        {1, 0, 0, 0},
+        {0, cos(theta), -sin(theta), 0},
+        {0, sin(theta), cos(theta), 0},
+        {0, 0, 0, 1}
+      };
+      pt = Translate_laser_mounting_plate * Phi_laser_mounting_plate * Theta_laser_mounting_plate * pt;
+
+      geometry_msgs::msg::Point32 point;
+      point.x = static_cast<float>(pt[0]);
+      point.y = static_cast<float>(pt[1]);
+      point.z = static_cast<float>(pt[2]);
+      pc.points.push_back(point);
+    }
+    angle += msg->angle_increment;
+  }
 
   // publish transformated point cloud
   point_cloud_pub_->publish(pc);
