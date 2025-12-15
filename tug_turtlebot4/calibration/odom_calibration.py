3a8bd893c0dd941499cceef59c2b6deb6fab072f
Christoph Royer <mail@christophroyer.com>
2025-12-15 12:58:06 +0100

    add plots etc

--- tug_turtlebot4/calibration/odom_calibration.py
+++ tug_turtlebot4/calibration/odom_calibration.py
@@ -5,6 +5,7 @@ from matplotlib.axes import Axes
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
+TICKS_PER_REV = 2573
 
 def read_pose_data(filename):
     data = np.loadtxt(filename)
@@ -33,7 +34,7 @@ def draw_arrow(ax: Axes, x: float, y: float, theta: float, length: float = 0.05,
     ax.arrow(x, y, dx, dy, head_width=0.005, head_length=0.005, fc=color, ec='none')
 
 
-def visualize_data(ax, pose_data, color='1'):
+def visualize_poses(ax, pose_data, color='1'):
     ax.plot(pose_data['x'], pose_data['y'], color=color)
 
     # Draw arrows for every nth point to show orientation
@@ -43,11 +44,11 @@ def visualize_data(ax, pose_data, color='1'):
     for i in range(0, len(pose_data['x'])):
         draw_arrow(ax, pose_data['x'][i], pose_data['y'][i], pose_data['theta'][i], color=color)
 
-
-def encoders_to_poses(encoder_data, init_x=0.0, init_y=0.0, init_yaw=0.0, ticks_per_rev=2573, wheel_diam=0.0715, wheel_base=0.233):
+def encoders_to_poses(encoder_data, init_x=0.0, init_y=0.0, init_yaw=0.0, wheel_diam=0.0715, wheel_base=0.233):
+    global TICKS_PER_REV
     wheel_circumference = wheel_diam * np.pi
-    dist_left = encoder_data['encoder_left'] * wheel_circumference / ticks_per_rev
-    dist_right = encoder_data['encoder_right'] * wheel_circumference / ticks_per_rev
+    dist_left = encoder_data['encoder_left'] * wheel_circumference / TICKS_PER_REV
+    dist_right = encoder_data['encoder_right'] * wheel_circumference / TICKS_PER_REV
     delta_s = (dist_left + dist_right) / 2
     delta_theta = (dist_right - dist_left) / wheel_base
 
@@ -66,6 +67,12 @@ def encoders_to_poses(encoder_data, init_x=0.0, init_y=0.0, init_yaw=0.0, ticks_
 
     return data
 
+def optimise_odometry(encoder_data, pose_data):
+    wheel_speeds_left = encoder_data['encoder_right'] / TICKS_PER_REV * 2 * np.pi
+    wheel_speeds_right = encoder_data['encoder_left'] / TICKS_PER_REV * 2 * np.pi
+    print("TODO: Implement odometry optimisation.")
+    return
+
 def main():
     # Read data files
     pose_data = read_pose_data('../../../../pose.dat')
@@ -74,17 +81,29 @@ def main():
     # Convert encoder data to pose data
     odom_pose_data_naive = encoders_to_poses(odom_data)
 
+    # Optimise odometry
+    odom_pose_data_optimised = optimise_odometry(odom_data, pose_data)
+
     # Visualize the data
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8))
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.set_xlabel('X Position')
-    ax.set_ylabel('Y Position')
-    ax.set_title('Robot Position')
-    ax.grid(True)
+    # Position plot
+    ax1.set_xlabel('X Position')
+    ax1.set_ylabel('Y Position')
+    ax1.set_title('Robot Position')
+    ax1.grid(True)
+    visualize_poses(ax1, pose_data, 'blue')
+    visualize_poses(ax1, odom_pose_data_naive, 'red')
 
-    visualize_data(ax, pose_data, 'blue')
-    visualize_data(ax, odom_pose_data_naive, 'red')
+    # Yaw angle plot
+    ax2.set_ylabel('Yaw Angle')
+    ax2.set_xlabel('Time (s)')
+    ax2.grid(True)
+    ax2.plot(pose_data['stamp'], pose_data['theta'], color='blue')
+    ax2.plot(odom_pose_data_naive['stamp'], odom_pose_data_naive['theta'], color='red')
+    ax2.legend(['Ground Truth', 'Odometry (naive)'])
+
+    plt.tight_layout()
 
     plt.show()
 
